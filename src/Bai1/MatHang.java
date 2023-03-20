dfc8c77e603497c7268d48a8b4990117f8987530
phucdang24 <106759480+phucdang24@users.noreply.github.com>
2023-03-21 00:07:51 +0700

    Add gia property

--- src/Bai1/MatHang.java
+++ src/Bai1/MatHang.java
@@ -25,20 +25,32 @@ public class MatHang {
         this.nhaSanSuat = nhaSanSuat;
     }
 
+    public float getGia() {
+        return gia;
+    }
+
+    public void setGia(float gia) {
+        this.gia = gia;
+    }
+
     public MatHang(){
         this.maHang = null;
         this.tenHang = null;
         this.nhaSanSuat = null;
+        this.gia = -1f;
     }
 
-    public MatHang(String maHang, String tenHang, String nhaSanSuat) {
+    public MatHang(String maHang, String tenHang, String nhaSanSuat, float gia) {
         this.maHang = maHang;
         this.tenHang = tenHang;
         this.nhaSanSuat = nhaSanSuat;
+        this.gia = gia;
     }
 
     private String maHang;
     private String tenHang;
     private String nhaSanSuat;
+    private float gia;
+
 
 }
